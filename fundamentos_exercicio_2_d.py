d6a9870ffb4639fadb544de136cd28009d66a90a
Glauco Rocha <251035158@aluno.unb.br>
2026-09-05 18:58:51 -0300

    Ajustes

--- fundamentos_exercicio_2_d.py
+++ fundamentos_exercicio_2_d.py
@@ -4,14 +4,14 @@ DESC = 5/100
 if km <= 0:
   print("Distância inválida")
 elif km <= 50:
-  total = TX_MIN + km * 1.75
+  total = TX_MIN+km*1.75
 elif km <= 150:
-  total = TX_MIN + 50 * 1.75 + (km - 50) * 1.65
+  total = TX_MIN+50*1.75+(km-50)*1.65
 else:
-  total = TX_MIN + 50 * 1.75 + 100 * 1.65 + (km - 150) * 1.50
+  total = TX_MIN+50*1.75+100*1.65+(km-150)*1.50
 if km > 0:
   if km > 300:
-    total -= total * DESC
-  medio = total / km
+    total -= total*DESC
+  medio = total/km
   print(f"Total: R$ {total:.2f}")
   print(f"Médio: R$ {medio:.2f}/km")
